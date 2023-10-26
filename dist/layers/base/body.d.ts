cefaba46086221cbf085129f61fc891c01c8c3b2
Kamilo Vasquez <killo.blanco@gmail.com>
2023-10-26 01:36:20 -0500

    📦 Config package publisher

--- dist/layers/base/body.d.ts
+++
@@ -1,2 +0,0 @@
-import type { CSSRuleObject, PluginAPI } from 'tailwindcss/types/config';
-export declare const Body: (theme: PluginAPI['theme']) => CSSRuleObject;
--- dist/layers/base/index.d.ts
+++
@@ -1,2 +0,0 @@
-import type { PluginAPI } from 'tailwindcss/types/config';
-export declare const buildBaseLayer: ({ addBase, theme }: PluginAPI) => void;
